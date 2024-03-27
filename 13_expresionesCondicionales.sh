c9b6e210520115c4881f6e0c3296d40fc01acfc7
ACBC <acbc.dev@gmail.com>
2024-03-27 11:04:54 -0500

    Refactor input prompts and add input validation in 13_expresionesCondicionales.sh

--- 13_expresionesCondicionales.sh
+++ 13_expresionesCondicionales.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
 # Programa para ejemplificar el uso de expresiones codicionales
-
-read -p "Ingrese su edad: " edad
-read -p "Ingrese su país: " pais
-read -p "Ingrese el path de un archivo: " pathArchivo
+read -rp "Ingrese su edad: " edad
+read -rp "Ingrese su país: " pais
+read -rp "Ingrese el path de un archivo: " pathArchivo
 
 echo -e "\nExpresiones condicionales con números"
-if [ $edad -lt 10 ]; then
+if [ "$edad" -lt 10 ]; then
   echo "La persona es un niño o niña"
-elif [ $edad -ge 10 ] && [ $edad -le 17 ]; then
+elif [ "$edad" -ge 10 ] && [ "$edad" -le 17 ]; then
   echo "La persona es un adolescente"
 else
   echo "La persona es mayor de edad"
 fi
 
 echo -e "\nExpresiones condicionales con números"
-if [ $pais == "EEUU" ]; then
+if [ "$pais" == "EEUU" ]; then
   echo "La persona es estadounidense"
-elif [ $pais == "Ecuador" ] || [ $pais == "Colombia" ]; then
+elif [ "$pais" == "Ecuador" ] || [ "$pais" == "Colombia" ]; then
   echo "La persona es de Sudamérica"
 else
   echo "Se desconoce la nacionalidad"
 fi
 
 echo -e "\nExpresiones condicionales con archivos"
-if [ -d $pathArchivo ]; then
+if [ -d "$pathArchivo" ]; then
   echo "El directorio $pathArchivo existe"
 else
   echo "El directorio $pathArchivo no existe"
