2ad52c2b3c566b2d0940d7c133b2d69689e9c581
ACBC <acbc.dev@gmail.com>
2024-03-28 15:56:59 -0500

    Add script for handling case statements in Bash

---
+++ 14-setenciasCase.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Programa para ejemplificar el uso de la sentencia case
+
+echo "Ejemplo sentencia case"
+read -p "Ingrese una opción de la A - Z: " opcion
+echo -e "\n"
+
+case $opcion in
+"A") echo -e "\nOperación Guardar Archivo" ;;
+"B") echo -e "\nOperación Eliminar Archivo" ;;
+[C-E]) echo -e "\nNo está implementada la operación" ;;
+*) echo -e "\nOpción incorrecta" ;;
+esac
